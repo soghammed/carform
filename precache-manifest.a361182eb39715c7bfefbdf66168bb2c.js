edc6c7d4afe0cb0c9f0fc112b56a3504b54f7788
Mohammed hammed <devhammed@gmail.com>
2020-06-04 21:55:48 +0100

    update3

---
+++ precache-manifest.a361182eb39715c7bfefbdf66168bb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f57d4c6eee8d89cd08e58449c3fe77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5044ace07d188ee03e4",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "3169badef0eb40c46672",
+    "url": "/static/css/main.7b597fad.chunk.css"
+  },
+  {
+    "revision": "d5044ace07d188ee03e4",
+    "url": "/static/js/2.cbc46b31.chunk.js"
+  },
+  {
+    "revision": "5d8326d3efa676d3ffe075800bbd250c",
+    "url": "/static/js/2.cbc46b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3169badef0eb40c46672",
+    "url": "/static/js/main.705585b8.chunk.js"
+  },
+  {
+    "revision": "54129674d7321189f64b",
+    "url": "/static/js/runtime-main.fae5c022.js"
+  }
+]);
